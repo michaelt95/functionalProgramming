6fb57f9b65744f72149ccbce31c9878dae1cec7d
michaelt95 <alexander04315@gmail.com>
2020-05-09 22:39:32 +0200

    metodos por referencia estatico

--- src/Stream/referencia/Main.java
+++ src/Stream/referencia/Main.java
@@ -2,20 +2,22 @@ package Stream.referencia;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args){
-        // Obtener el cuadrado de todos los numeros en la lista.
-        // El método map nos permite transformar un objeto en otro a través de una función.
+        // Referencia a metodos estaticos
+        // Imprimir en consola el cubo de los elementos en la lista
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10); // 1 lista
-
-        List<Integer> result = numeros.stream() // 2 Abstraccion
-                .map(num -> num*num) // 3
-                .collect(Collectors.toList()); // 4 Segunda lista
-
-        System.out.println(result);
+        numeros.stream()
+                .map(Main::ToCube)
+                .forEach(System.out::println);
     }
+
+    private static int ToCube(Integer numero) {
+        return numero*numero*numero;
+    }
+
+
 }
