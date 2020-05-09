8fb638998cd8e7a0cfe67c4bb4d857bdc2afde8d
michaelt95 <alexander04315@gmail.com>
2020-05-09 17:11:31 +0200

    el compilador infiere el tipo de los parametros

--- src/lambdas/Main.java
+++ src/lambdas/Main.java
@@ -3,7 +3,10 @@ package lambdas;
 public class Main {
 
     public static void main(String[] args){
-        ISaludar saludar = (usuario) -> {System.out.println("Hola "+usuario);};
+        // El compilador infiere el tipo de los parametros
+        // Podemos modificar el nombre de los parametros
+
+        ISaludar saludar = (nombre) -> {System.out.println("Hola "+nombre);};
         saludar.Saluda("mike");
     }
 }
