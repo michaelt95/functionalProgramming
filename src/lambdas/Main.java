58583173c6621b75f8536e0e663810ae1ea59e76
michaelt95 <alexander04315@gmail.com>
2020-05-09 17:12:10 +0200

    Siempre que tengamos un solo parametro podemos omitir el uso de parentesis

--- src/lambdas/Main.java
+++ src/lambdas/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args){
         // El compilador infiere el tipo de los parametros
         // Podemos modificar el nombre de los parametros
-
-        ISaludar saludar = (nombre) -> {System.out.println("Hola "+nombre);};
+        //Siempre que tengamos un solo parametro podemos omitir el uso de parentesis
+        ISaludar saludar = nombre -> {System.out.println("Hola "+nombre);};
         saludar.Saluda("mike");
     }
 }
